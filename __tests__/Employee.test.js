9da6bd5c2e819d2962b4c36b2a3768ebbc65f778
George <gms1980@icloud.com>
2021-04-02 12:53:23 -0700

    initial commit

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,11 @@
+
+const Employee = require ('../lib/Employee');
+
+
+const employee = new Employee('Stark, Tony', 1);
+    console.log(employee);
+test('creates a random employee object', () => {
+    expect(employee.name).toBe(employee.name, 'Stark, Tony');
+    expect(employee.id).toEqual(expect.any(Number));;
+    expect(employee.email).toBe(employee.email, '@');  
+  });
