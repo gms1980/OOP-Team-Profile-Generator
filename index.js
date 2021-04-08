e942fe5b90162b209898d5de6b4d0468845eeccb
George <gms1980@icloud.com>
2021-04-08 09:51:52 -0700

    added prompts to init questions array

--- index.js
+++ index.js
@@ -1,20 +1,52 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
+const fs = require("fs");
+const inquirer = require("inquirer");
 
-const Employee = require('./lib/Employee');
-const Manager = require('./lib/Manager');
-const Intern = require('./lib/Intern');
-const Engineer = require('./lib/Engineer');
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
+const Engineer = require("./lib/Engineer");
 
-inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is the Employees Name?'
+function addStaff() {
+  inquirer
+    .prompt([
+       { 
+        type: "input",
+        name: "name",
+        message: "Enter Staff members name?"        
+      },
+      {
+          type: "list",
+          name: "role",
+          message: "Select the role of the staff member?",
+          choices: ["Employee", "Engineer", "Intern", "Manager"],
+      },
+      { 
+        type: "input",
+        name: "id",
+        message: "Enter staff members ID?",        
+      },
+
+     ])
+
+    .then(function({name, role, id, email}) {
+        let roleInfo = "";
+        if (role === "Engineer") {
+            roleInfo = "GitHub username";            
         }
-    ])
+        else if (role === "Manager") {
+        roleInfo = "office phone number";
+        }
+    })    
 
-    .then(answers => {
-        console.log(answers);
-    })
-    
+
+    // .then((answers) => {
+    //   console.log(answers);
+    // })
+}
+
+function initApp() {
+    addStaff();
+    //startHTML
+}
+
+initApp();
