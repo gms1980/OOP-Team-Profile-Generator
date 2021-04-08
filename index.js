9fa008104391701d74d60ea1d870a41c135f1fa3
George <gms1980@icloud.com>
2021-04-08 14:50:13 -0700

    working writeToFile

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-const generatePage = require('./utils/generateHTML')
+const generatePage = require('./utils/generate-site')
 
 const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
 const Engineer = require("./lib/Engineer");
+const { SIGTERM } = require("constants");
 
 //Questions Array
 // function addStaff() {
---
+++ utils/generate-site.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const { copyFile } = require('fs');
+
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) =>{
+        //if there's an err, reject the Promise and send the error to the promise's `.catch()` method
+        if (err) {
+            reject(err);
+            // return out fo the function here to makke sure the Promise doesn't accidently execute the resole() function as well
+            return;
+        }
+        // if everything went well, resolve the Promise and send the success data to the `.then()` method
+    resolve({
+        ok: true,
+        message: 'File created!'
+
+    });
+    });
+};
+
+const copyfile = fileContent => {
+    return new Promise((resolve, reject) => {
+        if (err) {
+            reject(err);
+            return;
+        }
+    resolve({
+        ok: true,
+        message: 'file created'
+    });
+    });
+}
+
+module.exports = {writeFile, copyfile: copyFile};
