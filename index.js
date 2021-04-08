b1ee0f523045e026a287d522c15d46848067a851
George <gms1980@icloud.com>
2021-04-08 10:12:20 -0700

    attempting additional questions array

--- index.js
+++ index.js
@@ -6,47 +6,55 @@ const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
 const Engineer = require("./lib/Engineer");
 
+
 function addStaff() {
   inquirer
     .prompt([
-       { 
+      {
         type: "input",
         name: "name",
-        message: "Enter Staff members name?"        
+        message: "Enter Staff members name?",
       },
       {
-          type: "list",
-          name: "role",
-          message: "Select the role of the staff member?",
-          choices: ["Employee", "Engineer", "Intern", "Manager"],
+        type: "list",
+        name: "role",
+        message: "Select the role of the staff member?",
+        choices: ["Employee", "Engineer", "Intern", "Manager"],
       },
-      { 
+      {
         type: "input",
         name: "id",
-        message: "Enter staff members ID?",        
+        message: "Enter staff members ID?",
       },
+    ])
 
-     ])
-
-    .then(function({name, role, id, email}) {
-        let roleInfo = "";
-        if (role === "Engineer") {
-            roleInfo = "GitHub username";            
-        }
-        else if (role === "Manager") {
+    .then(function ({ name, role, id, email }) {
+      let roleInfo = "";
+      if (role === "Engineer") {
+        roleInfo = "GitHub username";
+      } else if (role === "Manager") {
         roleInfo = "office phone number";
-        }
-    })    
-
-
-    // .then((answers) => {
-    //   console.log(answers);
-    // })
+      } else if (role === "Intern") {
+        roleInfo = "School Name";
+      }
+    });
 }
+    // inquirer.prompt ([
+    //     {
+    //         type: "input",
+    //         name: "roleInfo",
+    //         message: `Enter staff member's ${roleInfo}`,
+    //     }
+    // ])
+
+  // .then((answers) => {
+  //   console.log(answers);
+  // })
+//}
 
 function initApp() {
-    addStaff();
-    //startHTML
+  addStaff();
+  //startHTML
 }
 
 initApp();
