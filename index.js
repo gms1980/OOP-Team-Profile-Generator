2b6b059f62e6ce68b950b7dc504fcd411515c477
George <gms1980@icloud.com>
2021-04-10 16:49:17 -0700

    adding html

--- index.js
+++ index.js
@@ -12,8 +12,9 @@ const Engineer = require("./lib/Engineer");
 // function addStaff() {
 //   inquirer
 //     .prompt([
-const questions = [
-  {
+function addMember() {
+    inquirer.prompt([
+{
     type: "input",
     name: "name",
     message: "Enter Staff members name?",
@@ -36,7 +37,7 @@ const questions = [
   },
   {
     type: "input",
-    message: "what school does Employee attend?",
+    message: "what school does member attend?",
     name: "school",
     when: function (answers) {
       return answers.Title === "Intern";
@@ -44,7 +45,7 @@ const questions = [
   },
   {
     type: "input",
-    message: "what is Employee office number?",
+    message: "what is Managers office number?",
     name: "officeNumber",
     when: function (answers) {
       return answers.Title === "Manager";
@@ -58,47 +59,83 @@ const questions = [
       return answers.Title === "Engineer";
     },
   },
-//   {
-//     type: "list",
-//     name: "addMembers",
-//     message: "Would you like to add any additional members?",
-//     choices: ["Yes", "No"],
-//     when: function (answers) {
-//         return answers.Title ==="Questions"
-//     }
-//   },
-];
+
+])}
+
+  //   {
+  //     type: "list",
+  //     name: "addMembers",
+  //     message: "Would you like to add any additional members?",
+  //     choices: ["Yes", "No"],
+  //     when: function (answers) {
+  //         return answers.Title ==="Questions"
+  //     }
+  //   },
 
 
-// function writeToFile(fileName, data) {
-//   fs.writeFile(fileName, data, (err) => {
-//     if (err) throw err;
-//     console.log("please work!");
-//   });
-// }
-
-
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) throw err;
+    console.log("please work!");
+  });
+}
 
 //Function to initiate the app initApp
 function initApp() {
-  inquirer.prompt(questions).then(function (userInput) {
-    console.log(userInput);
-    writeToFile("./dist/index.html", generateHTML, function(err) {
-      if (err) {
-        console.log(err);
-      }
-    });
-    console.log(start);
-  });
-  //startHTML
+    addMember();    
 }
-
 initApp();
 
-// fs.writeFile("./dist/index.html", html, function(err) {
-//     if (err) {
-//         console.log(err);
-//     }
-// });
-// console.log("start");
-// }
+
+function startHTML() {
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Portfolio Demo</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css"
+        />
+        <!-- CSS only -->
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6"
+          crossorigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="stylesheet" href="style.css" />
+      </head>
+      <body>
+        <div class="card" style="width: 18rem">
+          <div class="card-body">
+            <h5 class="card-title">${name}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+          </div>
+        </div>`
+        fs.writeFile("./dist/index.html", html, function(err) {
+            if (err) {
+                console.log(err);
+            }
+        });
+        console.log("start");
+}
+
+fs.appendFile("/dist/index.html", data, function (err) {
+    if (err) {
+        console.log(err);
+
+    }
+})
--- utils/generate-site.js
+++ utils/generate-site.js
@@ -3,50 +3,28 @@ const { copyFile } = require('fs');
 
 
 
-function generateHTML() {
-  const generateHTML = `<!DOCTYPE html>
-<html lang="en">
- <head>
-     <meta charset="UTF-8">
-     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Document</title>
- </head>
- <body>
-     <h4>look at this</h4>
- </body>
- </html>`;
+const generateSite = questions => {
+  return 
 }
 
+// for (let i = 0; i < array.length; i++) {
+//   // array[i] is the object for the employee
+//  `<p>${array[i].employeeName}</p>
+//  <p>${array[i].employeeType}</p>
+// `}
 
-const writeFile = fileContent => {
-    return new Promise((resolve, reject) =>{
-        //if there's an err, reject the Promise and send the error to the promise's `.catch()` method
-        if (err) {
-            reject(err);
-            // return out fo the function here to makke sure the Promise doesn't accidently execute the resole() function as well
-            return;
-        }
-        // if everything went well, resolve the Promise and send the success data to the `.then()` method
-    resolve({
-        ok: true,
-        message: 'File created!'
+// function generateSite() {
+//  const generateSite = `<!DOCTYPE html>
+// <html lang="en">
+//  <head>
+//      <meta charset="UTF-8">
+//      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//      <title>Document</title>
+//  </head>
+//  <body>
+//      <p>${userInput}</p>
+//  </body>
+//  </html>`
+// }
 
-    });
-    });
-};
-
-const copyfile = fileContent => {
-    return new Promise((resolve, reject) => {
-        if (err) {
-            reject(err);
-            return;
-        }
-    resolve({
-        ok: true,
-        message: 'file created'
-    });
-    });
-}
-
-module.exports = {writeFile, copyfile: copyFile};
