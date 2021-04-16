56a2334d22bc19bc0b7237fa6684329c5e937650
George <gms1980@icloud.com>
2021-04-16 14:38:01 -0700

    added final code

--- index.js
+++ index.js
@@ -146,25 +146,24 @@ const buildHtml = (data) => {
           <div class="col-4 mt-4">
           <div class="card h-100">
               <div class="card-header">
-                  <h3>${Engineer.name}</h3>
+                  <h3>${data[0].name}</h3>
                   <h4>Engineer</h4><i class="material-icons">glasses</i>
               </div>
               <div class="card-body">
-                  <p class="id">ID: ${Engineer.id}</p>
+                  <p class="id">ID: ${data[1].id}</p>
                   <p class="email">Email: <a href="mailto:${Engineer.email}">${Engineer.email}</a></p>
-                  <p class="github">Github: <a href="https://github.com/${Engineer.github}">${Engineer.github}</a></p>
+                  <p class="github">Github: <a href="https://github.com/.github}">${Engineer.github}</a></p>
               </div>
           </div>
           </div>
       <div class="col-4 mt-4">
       <div class="card h-100">
           <div class="card-header">
-              
+          <h3>${data[0].name}</h3>
               <h4>Intern</h4><i class="material-icons">assignment_ind</i>
           </div>
           <div class="card-body">
-              <p class="name">Name: ${Intern.name}</p>
-              <p class="id">ID: ${Intern.id}</p>
+              <p class="id">ID: ${data[1].id}</p>
               <p class="email">Email:<a href="mailto:${Intern.email}">${Intern.email}</a></p>
               <p class="school">School: ${Intern.school}</p>
           </div>
@@ -173,11 +172,11 @@ const buildHtml = (data) => {
       <div class="col-4 mt-4">
       <div class="card h-100">
           <div class="card-header">
-              <h3>${Manager.name}</h3>
+              <h3>${data[0].name}</h3>
               <h4>Manager</h4><i class="material-icons">content_paste</i>
           </div>
           <div class="card-body">
-              <p class="id">ID: ${Manager.id}</p>
+              <p class="id">ID: ${data[1].id}</p>
               <p class="email">Email: <a href="mailto:${Manager.email}">${Manager.email}</a></p>
               <p class="office">Office Number: ${Manager.officeNumber}</p>
           </div>
